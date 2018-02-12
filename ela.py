46063ff8d75de174df48ed69d129bc4c691dafed
larryzhao <larrylzhao@gmail.com>
2018-02-11 23:38:10 -0600

    ELA - compression

--- ela.py
+++ ela.py
@@ -1,18 +1,27 @@
 from PIL import Image
+from glob import glob
+import ntpath
+import os
 
 class ela:
 
-    def __init__(self):
-        self.extract_dir = ['Au', 'Sp']
+    def __init__(self, images):
+        self.images = images
 
-    def compress(self, imgPath):
-        img = Image.open(imgPath)
-        IMAGE_10 = os.path.join('./images/dog10.jpeg')
-        im1.save(IMAGE_10,"JPEG", quality=10)
-        im10 = Image.open(IMAGE_10)
+    def compress(self, outPath, qualityVal):
+        for img in self.images:
+            image = Image.open(img)
+            image.save(outPath+os.sep+ntpath.basename(img),"JPEG", quality=qualityVal)
 
-    def main(self):
-        pass
 
-ela.main()
+
+def main():
+    extract_dir = ['Au', 'Sp']
+    for each_dir in extract_dir:
+        img_list = glob('casia1'+os.sep+each_dir+os.sep+'*.jpg')
+        outPath = 'ela'+os.sep+'compress'
+        elaIns = ela(img_list)
+        elaIns.compress(outPath, 90)
+
+main()
 
