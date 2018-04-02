693598ef06910173d54c9fb5c0fe22046f8d92ba
zhali28 <lingyuan.zhao@ca.com>
2018-04-02 18:23:06 -0500

    implemented CNN

---
+++ models/cnn.py
@@ -0,0 +1,95 @@
+from PIL import Image
+import random
+from glob import glob
+import ntpath
+import os
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+
+
+class cnn:
+
+    def __init__(self):
+        self.outPath = 'cnn'+os.sep+'CASIA_V2'+os.sep
+
+    def get_out_path(self):
+        return self.outPath
+
+    @staticmethod
+    def strip_spaces(imgPath):
+        # strip out spaces in image path names
+        img_list = glob(imgPath + '/*')
+        print("image path: ")
+        print(img_list)
+        for imagePath in img_list:
+            print(imagePath)
+            os.rename(imagePath, imagePath.replace(" copy", ""))
+
+    @staticmethod
+    def random_crop(imgPath, height, width):
+        img = Image.open(imgPath)
+
+        # check if the image dimensions are at least as big as the crop size
+        imgWidth, imgHeight = img.size
+        if (imgWidth < width) or (imgHeight < height):
+            raise Exception('Image size is too small for the crop size')
+        else:
+            x = random.randint(0, imgWidth - width)
+            y = random.randint(0, imgHeight - height)
+
+            img = img.crop((x, y, x + width, y + height))
+            return img
+
+    @staticmethod
+    def build_model(width, height, depth, classes):
+        # initialize the model
+        model = Sequential()
+        inputShape = (height, width, depth)
+
+        model.add(Conv2D(30, kernel_size=(5, 5), strides=(2, 2),
+                         activation='relu',
+                         input_shape=inputShape))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(classes, activation='softmax'))
+
+        # return the constructed network architecture
+        return model
+
+
+# def main():
+#     cnn.strip_spaces('resources/casia2/Tp_patches')
+#
+#     # img_list = glob('casia2'+os.sep+'Au'+os.sep+'*')
+#     # cnnIns = cnn()
+#     # outDir = cnnIns.get_out_path() + 'Au_cropped'
+#     # if not os.path.exists(outDir):
+#     #     os.makedirs(outDir)
+#     # for imgPath in img_list:
+#     #     img = cnnIns.random_crop(imgPath, 128, 128)
+#     #     fName = outDir+os.sep+ntpath.basename(imgPath)
+#     #     fName = os.path.splitext(fName)[0]+'.jpg'
+#     #     img.save(fName, "JPEG")
+#
+#     # img_list = glob('casia2'+os.sep+'Tp'+os.sep+'*')
+#     # cnnIns = cnn()
+#     # outDir = cnnIns.getOutPath() + 'Tp_cropped'
+#     # if not os.path.exists(outDir):
+#     #     os.makedirs(outDir)
+#     # for imgPath in img_list:
+#     #     img = cnnIns.random_crop(imgPath, 128, 128)
+#     #     fName = outDir+os.sep+ntpath.basename(imgPath)
+#     #     fName = os.path.splitext(fName)[0]+'.jpg'
+#     #     img.save(fName, "JPEG")
+#
+# main()
---
+++ train_model.py
@@ -0,0 +1,134 @@
+# modeled after https://www.pyimagesearch.com/2017/12/11/image-classification-with-keras-and-deep-learning/
+
+# USAGE
+# python train_network.py --dataset images --model santa_not_santa.model
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+from models.cnn import cnn
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import cv2
+import tensorflow as tf
+
+# try to fix bug with GPU tensorflow
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--auset", required=True,
+                help="path to input authentic dataset")
+ap.add_argument("-t", "--tpset", required=True,
+                help="path to input tampered dataset")
+ap.add_argument("-m", "--model", required=True,
+                help="path to output model")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+                help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+# initialize model parameters
+SPLIT = .75
+EPOCHS = 10
+LR = .001
+BS = 32
+
+# initialize the data and labels
+print("[INFO] loading images...")
+data = []
+labels = []
+
+# grab the image paths and randomly shuffle them
+auImagePaths = sorted(list(paths.list_images(args["auset"])))
+tpImagePaths = sorted(list(paths.list_images(args["tpset"])))
+
+random.seed(42)
+random.shuffle(auImagePaths)
+random.shuffle(tpImagePaths)
+
+# balance the datasets
+if (len(auImagePaths) < len(tpImagePaths)):
+    tpImagePaths = tpImagePaths[:len(auImagePaths)]
+else:
+    auImagePaths = auImagePaths[:len(tpImagePaths)]
+
+# create dataset and labels
+for imagePath in auImagePaths:
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (128, 128))
+    image = img_to_array(image)
+    data.append(image)
+    labels.append(0)
+for imagePath in tpImagePaths:
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (128, 128))
+    image = img_to_array(image)
+    data.append(image)
+    labels.append(1)
+
+# scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+# split dataset into training and test sets
+(trainX, testX, trainY, testY) = train_test_split(data,
+                                                  labels, test_size=1 - SPLIT, random_state=42)
+
+# convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes=2)
+testY = to_categorical(testY, num_classes=2)
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
+
+# initialize the model
+print("[INFO] compiling model...")
+model = cnn.build_model(width=128, height=128, depth=3, classes=2)
+opt = Adam(lr=LR, decay=LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network...")
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+                        validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+                        epochs=EPOCHS, verbose=1)
+
+# train model without using data augmentation
+# H = model.model.fit(trainX, trainY,
+# 					epochs=EPOCHS,
+# 					batch_size=BS,
+# 					verbose=1,
+# 					validation_data=(testX, testY))
+
+# save the model to disk
+print("[INFO] serializing network...")
+model.save(args["model"])
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+N = EPOCHS
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
